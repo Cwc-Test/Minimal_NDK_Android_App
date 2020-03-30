4b59ecc857580396a25728107980378310d7d683
maeiky <maeiky@gmail.com>
2020-03-30 01:41:30 -0400

    Convention

--- java/gz/GzCpp.java
+++ java/gz/GzCpp.java
@@ -7,13 +7,13 @@ public class GzCpp {
         System.loadLibrary( "GZE" );
     }
 
-    public static native long OnInit(int iWidth, int iHeight);
-    public static native void OnResize(long _oThis, int iWidth, int iHeight);
+    public static native long OnInit(int _nWidth, int _nHeight);
+    public static native void OnResize(long _oThis, int _nWidth, int _nHeight);
     public static native long OnRecreate(long _oThis);
     public static native void OnFrame(long _oThis);
     public static native void OnShutdown(long _oThis);
     
-    public static native void OnTouch(long _oThis, int iPointerID, float fPosX, float fPosY, int iAction );
+    public static native void OnTouch(long _oThis, int _nPointerID, float _nPosX, float _nPosY, int _nAction );
     
     public static native void OnPause(long _oThis);
     public static native void OnResume(long _oThis);
